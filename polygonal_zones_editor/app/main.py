7e37f375d4fb2e9265bab652b440f9f7069f0273
Michel Gerding <michelgerding@gmail.com>
2024-09-14 17:00:25 +0200

    minor refactor and added requirents.txt instead of hardcoding

--- polygonal_zones_editor/app/main.py
+++ polygonal_zones_editor/app/main.py
@@ -1,3 +1,4 @@
+import contextlib
 import json
 import os
 
@@ -56,22 +57,27 @@ def generate_static_file_routes(static_folder, prefix='/', options: dict = None)
 
         route_names[i] = path
 
-    route_names.remove('zones.json')
+    with contextlib.suppress(ValueError):
+        route_names.remove('zones.json')
+
     return [Route(prefix + static_file, static_file_route, methods=['GET']) for static_file in route_names]
 
 
-async def save_zones(request: Request) -> PlainTextResponse | JSONResponse:
-    """Saves the zones.json file."""
-    if not allow_request(opts, request):
-        _LOGGER.warning("Blocked request from %s on %s", request.client.host, request.url.path)
-        return PlainTextResponse('not allowed', status_code=403)
+def save_zones_generator(options: dict):
+    async def save_zones(request: Request) -> PlainTextResponse | JSONResponse:
+        """Saves the zones.json file."""
+        if not allow_request(options, request):
+            _LOGGER.warning("Blocked request from %s on %s", request.client.host, request.url.path)
+            return PlainTextResponse('not allowed', status_code=403)
 
-    geo_json = await request.json()
-    with open(ZONES_FILE, 'w') as f:
-        json.dump(geo_json, f)
-        _LOGGER.info("Saved zones.json")
+        geo_json = await request.json()
+        with open(ZONES_FILE, 'w') as f:
+            json.dump(geo_json, f)
+            _LOGGER.info("Saved zones.json")
 
-    return JSONResponse({'status': 'ok'})
+        return JSONResponse({'status': 'ok'})
+
+    return save_zones
 
 
 async def zones_json(_request: Request) -> JSONResponse:
@@ -86,20 +92,10 @@ async def zones_json(_request: Request) -> JSONResponse:
         })
 
 
-if __name__ == '__main__':
-    # ensure the zones.json file exists
-    os.makedirs(DATA_FOLDER, exist_ok=True)
-    if not os.path.exists(ZONES_FILE):
-        with open(ZONES_FILE, 'w') as f:
-            json.dump({"type": "FeatureCollection", "features": []}, f)
-
-    opts = load_options()
-    _LOGGER.info("Loaded options: %s", opts)
-    _LOGGER.error("Allow all ips: %s", allow_all_ips(opts))
-
-    # Generate the routes for the static files and the normal endpoints.
-    routes = generate_static_file_routes('static/', options=opts)
-    routes.append(Route('/save_zones', save_zones, methods=['POST']))
+def generate_app(options: dict) -> tuple[Starlette, dict]:
+    """Returns the Starlette app."""
+    routes = generate_static_file_routes('static/', options=options)
+    routes.append(Route('/save_zones', save_zones_generator(options), methods=['POST']))
     routes.append(Route('/zones.json', zones_json, methods=['GET']))
 
     # Disable logging for uvicorn
@@ -109,5 +105,21 @@ if __name__ == '__main__':
     log_config['loggers']['uvicorn.error']['handlers'] = []
     log_config['loggers']['uvicorn.access']['handlers'] = []
 
-    app = Starlette(debug=True, routes=routes)
-    uvicorn.run(app, host='0.0.0.0', port=8000, log_config=log_config, )
+    app = Starlette(debug=False, routes=routes)
+    return app, log_config
+
+
+if __name__ == '__main__':
+    # ensure the zones.json file exists
+    os.makedirs(DATA_FOLDER, exist_ok=True)
+    if not os.path.exists(ZONES_FILE):
+        with open(ZONES_FILE, 'w') as f:
+            json.dump({"type": "FeatureCollection", "features": []}, f)
+
+    options = load_options()
+    _LOGGER.info("Loaded options: %s", options)
+    _LOGGER.error("Allow all ips: %s", allow_all_ips(options))
+
+    # Generate the routes for the static files and the normal endpoints.
+    app, log_config = generate_app(options)
+    uvicorn.run(app, host='0.0.0.0', port=8000, log_config=log_config)
